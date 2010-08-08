4224c2ab83f61dd8ab9948f36d7d50f4431ad64b
Marcus Wyatt <marcus.wyatt@visfleet.com>
2010-08-08 22:10:17 +1200

    Improvements

--- bin/at_work.sh
+++ bin/at_work.sh
@@ -2,4 +2,4 @@
 
 defaults write com.apple.Xcode PBXCustomTemplateMacroDefinitions '{ORGANIZATIONNAME="Visfleet Ltd.";EMAIL="marcus.wyatt@visfleet.com";WEBADDRESS="http://www.visfleet.com";COMPANYNAME="Visfleet Ltd.";}'
 
-scp root@f2.vworkapp.com:/opt/apps/current/public/vwork.swf /Volumes/Data/Marcus/Work/git/vWork/public/vwork.swf
+scp root@qa-integrate.vworkapp.com:/opt/apps/current/public/vwork.swf /Volumes/Data/Marcus/Work/git/vWork/public/vwork.swf
--- irbrc
+++ irbrc
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-require 'rubygems'
+
 require 'pp'
 require 'yaml'
 require 'irb/completion'
